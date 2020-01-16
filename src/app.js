6fc4c249981cb5ad4e1f1b565d7a2ef28636cbed
Ian King <ian.antking@gmail.com>
2020-01-16 13:46:20 +0000

    reset app.js to basic hello-world

--- src/app.js
+++ src/app.js
@@ -1,24 +1,9 @@
 const express = require('express');
-const artistControllers = require('./controllers/artists');
-const albumControllers = require('./controllers/albums');
-const songController = require('./controllers/song');
 
 const app = express();
 
-app.use(express.json());
-
-app.post('/artists', artistControllers.create);
-
-app.get('/artists', artistControllers.list);
-
-app.get('/artists/:id', artistControllers.find);
-
-app.patch('/artists/:id', artistControllers.patch);
-
-app.delete('/artists/:id', artistControllers.delete);
-
-app.post('/artists/:artistId/albums', albumControllers.create);
-
-app.post('/album/:albumId/song', songController.postSong);
+app.get('*', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' });
+});
 
 module.exports = app;
